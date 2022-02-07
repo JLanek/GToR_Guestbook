c81f090f46c6b2cad2f85f64f48ac9e430cd3ab8
JLanek <landek.jakub@gmail.com>
2022-02-07 18:57:04 +0100

    Add working language selection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+    before_action :set_language
+
+    def set_language
+        I18n.locale = session[:locale] || I18n.default_locale
+    end
 end
--- app/controllers/language_controller.rb
+++ app/controllers/language_controller.rb
@@ -1,2 +1,6 @@
 class LanguageController < ApplicationController
+    def save_language
+        session[:locale] = params[:locale]
+        redirect_to :root
+    end
 end
