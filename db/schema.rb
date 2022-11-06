322ff549e4e9fcc117513f5936c233b55b03fe1e
JLanek <landek.jakub@gmail.com>
2022-11-06 13:46:17 +0100

    Add plpgsql in schema file

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_02_08_133528) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string "author"
     t.text "body"
